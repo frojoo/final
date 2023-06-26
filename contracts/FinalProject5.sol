67c4593db688ee76b58d76bb50e18f0d7f178e93
frojoo <wndyd29605@gmail.com>
2023-06-26 23:15:16 +0900

    final5

--- contracts/FinalProject5.sol
+++ contracts/FinalProject5.sol
@@ -29,32 +29,28 @@ contract NFT is ERC721Enumerable, Ownable {
     
     // 학생의 목표들
     mapping(address => Goal[]) public studentsGoals;
-    Goal[] allGoals;
+    // 학생이 서류 제출한 토큰 아이디들
+    uint[] submmitedGoals;
 
     // 초기 목표 설정(어떤 과목, 세부 목표, 시작일, 종료일, 달성여부, 선생님 승인여부, 민팅 승인여부) - 학생이 실행
     function setGoal(string memory _subject, string memory _detail, uint _start, uint _end) public {
         require(bytes(_detail).length > 0 && _start > 0 && _end > 0, "input value");
-        require(_start < _end, "End date has to be later");
 
         studentsGoals[msg.sender].push(Goal(msg.sender, _subject, _detail, _start, _end, ""));
-        allGoals.push(Goal(msg.sender, _subject, _detail, _start, _end, ""));
     }
 
-    // 선생님이 학생들의 목표 전체 보기
-    function allStudentsGoals() public view returns(Goal[] memory) {
-        return allGoals;
+    function getStudentsGoals(address _studentAddress) public view returns(Goal[] memory) {
+        return studentsGoals[_studentAddress];
     }
 
-
-    function mintNFT(address _teacher, string memory _metadataUri, uint _contentsNumber, string memory _url) public {
+    // 목표 달성에 대한 자료 제출
+    function submitEvidence(address _teacher, uint _contentsNumber, string memory _url) public {
         uint tokenId = totalSupply() + 1;
-    
-        require(isTeacher[_teacher], "msg.sender is not a Teacher");
-        require(bytes(_url).length > 0 && bytes(_metadataUri).length > 0, "input value");
+        
+        require(isTeacher[_teacher], "This address owner is not a Teacher");
+        require(bytes(_url).length > 0, "input value");
 
         studentsGoals[msg.sender][_contentsNumber - 1].proveUrl = _url;
-        _mint(msg.sender, tokenId);
-        metadataUris[tokenId] = _metadataUri;
         teachers[tokenId] = _teacher;
     }
 
@@ -69,10 +65,12 @@ contract NFT is ERC721Enumerable, Ownable {
     }
 
     // 토큰 아이디가 발행이 됐는지 확인을 해야함
-    function setAchievedByTeacher(uint _tokenId) public {
+    function mintNFT(address _studentAddress, uint _tokenId, string memory _metadataUri) public {
         require(msg.sender == teachers[_tokenId], "msg.sender is not my teacher");
-        require(isAchieved[_tokenId] == false);
+        require(bytes(_metadataUri).length > 0, "input vlaue");
         
+        metadataUris[_tokenId] = _metadataUri;
+        _mint(_studentAddress, _tokenId);
         isAchieved[_tokenId] = true;
     }
 
